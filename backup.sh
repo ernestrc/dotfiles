1592b1e81f815d2547063a2fb1e82fa620c5146f
Ubuntu <ubuntu@ip-172-31-25-146.eu-west-1.compute.internal>
2014-04-05 19:32:56 +0000

    fixed bug with backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,6 @@
 #TODO Function that checks if folder dotfiles exists in computer, if it doesnt. It creates it.
 echo "Starting dotfiles backup"
 cp $HOME/.zshrc $PWD/zshrc/.zshrc
-cp -rf $HOME/.dotfiles $PWD
-echo "Finished ZSH backup"
-echo "Finished VIM backup"
 git add --all .
 git commit -m "Backup through script"
 git push origin master
