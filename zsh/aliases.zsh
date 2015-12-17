6a40478fcc2263afd2b36fd1406a37ba4a9bef44
ErnestRC <ernest@unstable.build>
2015-12-16 23:16:41 -0800

    dotsync

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias reload!='. ~/.zshrc'
+alias grep='grep --color'
 
 function clean-steam {
     find ~/.steam/root/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" \) -print -delete && \
