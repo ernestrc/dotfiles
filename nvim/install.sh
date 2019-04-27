e0a2c1effbcf190d62519c271feacb28d0b8d3e6
ernestrc <ernest@unstable.build>
2019-04-26 18:27:19 -0700

    cleanup

--- nvim/install.sh
+++
@@ -1,7 +0,0 @@
-ln -s ~/.dotfiles/nvim ~/.config/
-
-if type "clang-format" &> /dev/null ; then
-  echo 'clang-format already installed!'
-else
-  echo 'please install clang-format for vim-autoformat!'
-fi
--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,2 @@
-EDITOR=nvim
+EDITOR=vim
 PYTHON=/usr/bin/python2.7
